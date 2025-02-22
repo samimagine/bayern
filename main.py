723906adfc4eaaa074a8d2e4e80a440883103b9e
Sam Imagine <sam@samimagine.com>
2025-02-22 22:55:50 +0100

    fix cors

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://samimagine.github.io/munich/"],
+    allow_origins=["http://localhost:5173", "http://samimagine.github.io", "http://samimagine.github.io/munich"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
